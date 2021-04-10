e0952a1a0c5328bf30ea9006ae3a66ad6f7e563b
Oscar Miguel Toro Torres <omigueltoro@gmail.com>
2021-04-10 11:21:54 -0500

    Se verifica variables nuleables

--- 1.variables/variable.dart
+++ 1.variables/variable.dart
@@ -2,7 +2,9 @@ void main() {
   String nombre = "Juan";
   int edad = 23;
   String telefono = "3134965969";
+  int? variabeAceptaNull = null;
 
   var apellido;
+  print(nombre);
   print(apellido);
 }
