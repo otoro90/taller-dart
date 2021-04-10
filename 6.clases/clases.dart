ea80ace77facc55636467cb074e3be135f5f8474
Oscar Miguel Toro Torres <omigueltoro@gmail.com>
2021-04-10 15:34:04 -0500

    Ejercicio con clases, contructores y su uso en herencia

--- 6.clases/clases.dart
+++ 6.clases/clases.dart
@@ -1,11 +1,29 @@
 class Persona {
-  String nombre;
-  String apellido;
-  int edad;
-  Persona(this.nombre, this.apellido, this.edad);
+  String? nombre;
+  String? apellido;
+  int? edad;
+  Persona({this.nombre, this.apellido, this.edad});
+
+  obtenerNombre() => this.nombre;
+}
+
+class Vehiculos {
+  int? puertas;
+  String? color;
+}
+
+class Empleado extends Persona {
+  String puesto;
+
+  Empleado(this.puesto) : super(nombre: "David", apellido: "Toro", edad: 30);
+
+  Empleado.formJson(this.puesto);
 }
 
 void main() {
-  var persona = Persona("Oscar", "Toro", 30);
+  var persona = Persona(nombre: "Oscar", apellido: "Toro", edad: 30);
   print(persona);
+
+  var empleado = Empleado.formJson("cajero");
+  print(empleado);
 }
