bff3443abcf5820ad7c1388aae48ded332c7a948
Oscar Miguel Toro Torres <omigueltoro@gmail.com>
2021-04-10 12:02:15 -0500

    Ejercicio con tipos con cadenas de texto

--- 2.tipos/tipos.dart
+++ 2.tipos/tipos.dart
@@ -8,4 +8,29 @@ void main() {
   } else {
     print(edad);
   }
+
+  //Strings
+  //
+  var texto1 = "";
+  var texto2 = "";
+  var texto3 = "";
+  var texto4 = "";
+
+  var texto5 = "suma de enteros en cadena ${4 + 5}";
+  print(texto5);
+
+  var texto6 = "valor1" + "valor2";
+  print(texto6);
+
+  var texto7 = "valor1" 'valor2';
+  print(texto7);
+
+  var texto8 = "texto a interpolar";
+  var numero1 = 50;
+  var texto9 =
+      "Se interpola \ '$texto8 \' con el valor de la variable numero 1 = $numero1";
+  print(texto9);
+
+  var texto10 = "It \n s easy to escape the string delimiter.";
+  print(texto10);
 }
